8883c8b781283667c86e4779d346a04e94aa6409
AmberSTR <118974621+AmberSTR@users.noreply.github.com>
2024-05-08 19:43:45 +0800

    auto search device number

--- initCAN.sh
+++ initCAN.sh
@@ -1,3 +1,7 @@
-sudo slcand -o -c -s8 /dev/ttyACM0 can0
+#!/bin/bash
+var="E"
+var=$(ls /dev|grep "ttyACM" )
+echo "Init device : "${var}
+sudo slcand -o -c -s8 /dev/${var} can0
 sudo ifconfig can0 up
 sudo ifconfig can0 txqueuelen 1000
